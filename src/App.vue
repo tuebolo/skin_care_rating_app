c77c6230ddf20160d5c88cd5839bec1dcfeda4ee
Isabelle Russell <issyruss2@gmail.com>
2021-05-26 13:38:57 +0200

    Create Home and Contact pages.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,54 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      fixed
+      v-model="drawer"
+      app
+    >
+    <v-list dense>
+      <router-link v-bind:to="{name: 'Home'}" class="side_bar_link">
+        <v-list-item-title>
+          <v-list-item-action>
+            <v-icon>home</v-icon>
+          </v-list-item-action>
+        </v-list-item-title>
+      </router-link>
+      <router-link v-bind:to="{ name: 'Contact'}" class="side_bar_link">
+            <v-list-item-title>
+              <v-list-item-action>
+                <v-icon>contact-mail</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>Contact</v-list-item-content>
+            </v-list-item-title>
+      </router-link>
+    </v-list>
+    </v-navigation-drawer>
+    <v-app-bar color="indigo" dark fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Home</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <div id="app">
+          <router-view/>
+        </div>
+      </v-container>
+    </v-main>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2021</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
+import './assets/stylesheets/main.css'
+
 export default {
-  name: 'App'
+  data: () => ({
+    drawer: null
+  }),
+  props: {
+    source: String
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
